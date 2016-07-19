00d17f713615f71a520b91ce5577742dec712858
fguenzi <fguenzi@egencia.com>
2016-07-19 09:46:54 +0200

    first commit with work in progress

---
+++ src/main/java/_10_Inner_Classes/_01_creating_inner_classes/aside_practice/HoldingClass.java
@@ -0,0 +1,34 @@
+package _10_Inner_Classes._01_creating_inner_classes.aside_practice;
+
+public class HoldingClass {
+
+    private static final int SF = 1;
+    private static int b = 2;
+    private final int c = 3;
+    private int i = 4;
+    public int p = 5;
+
+    static {
+        InnerClass a = new InnerClass();
+    }
+
+    static class InnerClass {
+
+        public void jojo() {
+            b = 1;
+        }
+    }
+
+    class InnerClass2{
+        private int p = 1;
+    }
+
+    public void jojo() {
+        InnerClass2 iC = new InnerClass2();
+    }
+
+    public static void jaja(){
+        InnerClass iC = new InnerClass();
+
+    }
+}
